40a6ba7eb8d22cefc5ea9c46c98f1c1a0ad3a6f8
Mike Dixon <mdixondesigns@gmail.com>
2024-06-28 16:44:14 -0400

    Adding basic print functionality
    * Added print function with demo styles
    * Added print buttons for testing

--- script.js
+++ script.js
@@ -7,3 +7,23 @@ avatar.addEventListener("click", dealwithit);
 function dealwithit() {
   avatar.classList.toggle("deal-with-it");
 }
+
+function printThis(){
+	var prtContent = document.getElementById("print-me");
+	var WinPrint = window.open('', '', 'left=0,top=0,width=800,height=900,toolbar=0,scrollbars=0,status=0');
+	// WinPrint.document.write(prtContent.innerHTML);
+  WinPrint.document.write('<html><head><title>Print</title>');
+	WinPrint.document.write('<style>');
+	WinPrint.document.write('body { font-family: Arial, sans-serif; }'); // Example style
+	WinPrint.document.write('#print-me { background-color: #f0f0f0; padding: 20px; }'); // Example style
+  WinPrint.document.write('.social-list li svg { display: inline-block; margin-right: 10px; vertical-align: middle; width: 32px;}');
+	WinPrint.document.write('</style>');
+	WinPrint.document.write('</head><body>');
+	WinPrint.document.write(prtContent.innerHTML);
+	WinPrint.document.write('</body></html>');
+	WinPrint.document.close();
+	WinPrint.document.close();
+	WinPrint.focus();
+	WinPrint.print();
+	WinPrint.close();
+}
