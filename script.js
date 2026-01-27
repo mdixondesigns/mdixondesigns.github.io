dd43dd462bf86068a83fdaf954f3e8a4a8a778dc
Mike Dixon <mdixondesigns@gmail.com>
2026-01-27 08:24:02 -0500

    Projects clean up
    * Removed glasses Easter egg from profile picture
    * Removed "scroll" behavior for project thumbnails
    * Removed unused project thumbnails
    * Removed children's museum and masters thesis projects
    * Updated stats for BB-8
    * Renamed profile picture image file
    * Updated copyright date

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-// Grab DOM elements for picture and glasses
-const avatar = document.getElementById("avatar-container");
-
-// Add event listener
-avatar.addEventListener("click", dealwithit);
-
-function dealwithit() {
-  avatar.classList.toggle("deal-with-it");
-}
+// Script will go here
